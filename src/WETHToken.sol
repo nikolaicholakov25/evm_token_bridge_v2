3a11ac5ad5fa2a7a158ce0ae2136a287512720a2
nikolay_cholakov <nikolaicholakov25@gmail.com>
2024-06-13 12:57:22 +0300

    update sample token contracts

--- src/WETHToken.sol
+++ src/WETHToken.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.19;
 
 import "./TokenBase.sol";
 
-contract WETHToken is TokenBase("WETHToken", "WETH") {}
+contract WETHToken is TokenBase {
+    constructor() TokenBase("WETHToken", "WETH", msg.sender) {}
+}
